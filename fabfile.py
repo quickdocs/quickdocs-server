491d069608483ea35f6ad714b8013dd590317465
Eitaro Fukamachi <e.arrows@gmail.com>
2015-07-22 00:02:13 +0900

    Modify fabfile to allow to deploy updater/extracter.

--- fabfile.py
+++ fabfile.py
@@ -2,16 +2,54 @@ from fabric.api import sudo, run, env, cd
 
 env.hosts = ['160.16.105.206']
 env.user = 'nitro-idiot'
-env.project_name = 'quickdocs'
-env.directory = '/srv/www/quickdocs-server'
 
-def update():
+#
+# Usage
+
+#   $ fab server deploy
+#   $ fab updater deploy
+#   $ fab extracter deploy
+
+#
+# Environments
+
+def server():
+    env.project_name = 'quickdocs-server'
+    env.directory = '/srv/www/quickdocs-server'
+
+def updater():
+    env.project_name = 'quickdocs-updater'
+    env.directory = '/srv/www/quickdocs-updater'
+
+def extracter():
+    env.project_name = 'quickdocs-extracter'
+    env.directory = '/srv/www/quickdocs-extracter'
+
+
+#
+# Deployment tasks
+
+def git_pull():
     with cd(env.directory):
         run('git pull')
+        run('git submodule update --recursive')
 
-def restart():
+def deploy_server():
+    git_pull()
     sudo('supervisorctl restart %s' % env.project_name, shell=False)
 
+def deploy_updater():
+    git_pull()
+
+def deploy_extracter():
+    git_pull()
+    with cd(env.directory):
+        run('scripts/update-docker-image')
+
 def deploy():
-    update()
-    restart()
+    if env.project_name == "quickdocs-server":
+        deploy_server()
+    elif env.project_name == "quickdocs-updater":
+        deploy_updater()
+    elif env.project_name == "quickdocs-extracter":
+        deploy_extracter()
